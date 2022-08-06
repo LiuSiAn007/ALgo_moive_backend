73a2141f9de341edd290c25f6b61db1a7d5c13c4
ALERI LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUAL17@OOCL.COM>
2022-08-06 16:54:31 +0800

    first commit

---
+++ src/main/java/com/algo/c3g2/C3g2Application.java
@@ -0,0 +1,13 @@
+package com.algo.c3g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C3g2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C3g2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/algo/c3g2/C3g2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algo.c3g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C3g2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
